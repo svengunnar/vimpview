54f2753af16ce8e70cb0116deb0f601517cae2ed
Mikael Nilsson <svengunnar.mikael@gmail.com>
2018-07-13 21:13:35 +0200

    Initial commit

---
+++ python/filemanager.py
@@ -0,0 +1,64 @@
+import subprocess
+import os.path
+
+class FileManager:
+    def __add_rec(self, l, d, last_key):
+        key = l.pop(0)
+
+        if not l:
+            d[last_key][1].append(key)
+        else:
+            if key not in d[last_key][0]:
+                d[last_key][0][key] = ({}, [])
+
+            self.__add_rec(l, d[last_key][0], key)
+
+    def __add(self, f):
+        l = []
+        while f != '':
+            l.append(os.path.basename(f))
+            f = os.path.dirname(f)
+
+        l = l[::-1]
+        self.__add_rec(l, self.root, "root")
+
+    def __gen_text_rec(self, d, l, indent):
+        for f in d[1]:
+            l.append(indent + f)
+
+        for key in d[0]:
+            l.append(indent + key + "/")
+            self.__gen_text_rec(d[0][key], l, indent + " ")
+
+    def get_result(self):
+        if  self.root == None:
+            return (None, None, None)
+
+        l = []
+        self.__gen_text_rec(self.root["root"], l, ""),
+        return (l, self.full_path, self.path)
+
+    def __init__(self):
+
+        # Find .git file
+        self.root = None
+        self.path = os.getcwd()
+        git_file = ".git"
+        while "/" != self.path:
+            if os.path.isdir(os.path.join(self.path, git_file)):
+                self.path = os.path.join(self.path, git_file)
+                break
+            self.path = os.path.dirname(self.path)
+
+        if self.path == "/":
+            return
+
+        self.root = {"root" : ({}, [])}
+        self.full_path = {}
+        files = subprocess.check_output(["git", "--git-dir", self.path, "ls-files", "-oc"])
+        files = files.splitlines()
+
+        for f in files:
+            self.full_path[os.path.basename(f)] = f
+            self.__add(f)
+
---
+++ python/vimpview.py
@@ -0,0 +1,103 @@
+from filemanager import FileManager
+import vim
+import os.path
+import copy
+
+def init():
+    a = FileManager()
+    text, full_path, git_path = a.get_result()
+    if text == None:
+        vim.command('''
+            function! Run()
+              python print("Found no .git dir!")
+            endfunction
+                ''')
+        vim.command("nnoremap <leader>o :call Run()<CR>")
+        return
+
+    vim.vars["g:text"] = text
+    vim.vars["g:full_path"] = full_path
+    vim.vars["g:path"] = os.path.dirname(git_path)
+
+    vim.command('''
+        function! OpenProjectView()
+          python open_project_view()
+        endfunction
+            ''')
+
+    vim.command('''
+        function! OpenFile()
+          python open_file()
+        endfunction
+            ''')
+
+    vim.command('''
+        function! PreQuit()
+          python pre_quit()
+        endfunction
+            ''')
+
+    vim.command("autocmd QuitPre * :call PreQuit()")
+    vim.command("nnoremap <leader>o :call OpenProjectView()<CR>")
+
+def open_project_view():
+    if "g:b_idx" in vim.vars:
+        # Check if the window is already opened in some window
+        # then we can just disregard this event.
+        for w in vim.windows:
+            if w.buffer.number == vim.vars["g:b_idx"]:
+                print("project view already opened")
+                return
+
+        vim.command("vert sb" + str(vim.vars["g:b_idx"]))
+        vim.current.window.width = 40
+    else:
+        vim.command("vnew")
+        vim.current.window.width = 40
+        t = vim.vars["g:text"]
+        for f in t:
+            vim.current.buffer.append(f)
+
+        vim.command("setlocal ro")
+        vim.command("setlocal hidden")
+        vim.command("setlocal nomodifiable")
+        vim.command("setlocal nonumber")
+
+        # highlight current line
+        vim.command("setlocal cursorline")
+        vim.command("color desert")
+        vim.command("setlocal cursorline")
+        vim.command("hi CursorLine term=bold cterm=bold guibg=Grey40")
+
+        # Map current buffer <CR> to open file
+        vim.command("nnoremap <buffer> <CR> :call OpenFile()<CR>")
+
+        vim.vars["g:b_idx"] = vim.current.buffer.number
+
+def open_file():
+    f = vim.current.line.strip()
+    d = vim.vars["g:full_path"]
+
+    if f not in d:
+        return
+
+    try:
+        if len(vim.windows) > 1:
+            vim.command("execute \"normal! \<c-w>b\"")
+            vim.command("split " + os.path.join(vim.vars["g:path"], d[f]))
+        else:
+            win = vim.current.window
+            vim.command("vsp " + os.path.join(vim.vars["g:path"], d[f]))
+            vim.command("execute \"normal! \<C-W>r\"")
+            win.width = 40
+    except vim.error:
+        return
+
+def pre_quit():
+    # clear the text in the buffer to avoid error msg
+    if (len(vim.windows) == 1):
+        if "g:b_idx" in vim.vars:
+            idx = str(vim.vars["g:b_idx"])
+            vim.command("bdelete! " + idx)
+            del vim.vars["g:b_idx"]
+
