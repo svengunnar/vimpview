9f3ddb67e3b7b1d32f4aff369d84437cf8c8c419
Mikael Nilsson <svengunnar.mikael@gmail.com>
2019-07-13 16:31:01 +0200

    done

--- python/getfiles.py
+++ python/getfiles.py
@@ -1,12 +1,15 @@
 import os
 import re
 
-def get_files_rec(out, regex, root_dir = ".", lvl = 0):
+def get_files_priv(out, regex, root_dir = ".", lvl = 0):
     ind = "  "
     for root, _ , files in os.walk(root_dir):
         lvl = len(root.split("/")) - 1
-        if lvl != 0:
+        files = list(filter(lambda f: regex.match(f), files))
+
+        if lvl != 0 and len(files) > 0:
             out.append(ind*(lvl - 1) + "{}/".format(os.path.basename(root)))
+
         for f in files:
             out.append(ind*lvl + "{}".format(f))
 
@@ -16,6 +19,11 @@ def get_files(pr_l, out):
     for pr in pr_l:
         proj_dir = os.path.expanduser(pr[0])
         common = os.path.commonpath([proj_dir, root_abs_path])
+        if proj_dir.endswith("/"):
+            proj_dir = proj_dir[:-1]
+        if common.endswith("/"):
+            common = common[:-1]
+
         if common == proj_dir:
             regex = re.compile(pr[1])
             os.chdir(proj_dir)
